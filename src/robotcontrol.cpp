b7d17ab0a4a527bc2919d160b3123cce90c9f21d
justinbar <121361660+justinbar@users.noreply.github.com>
2023-11-06 23:23:35 -0500

    Update robotcontrol.cpp

--- src/robotcontrol.cpp
+++ src/robotcontrol.cpp
@@ -5,7 +5,7 @@ class RobotController : public rclcpp::Node
 {
 public:
   RobotController()
-    : Node("control_robot_node"), loop_rate_(1) // based on loop_rate, the lower the number the robot will move longer period of time
+    : Node("control_robot_node"), loop_rate_(1) // a rate of 1 cycle per second
   {
     publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     RCLCPP_INFO(this->get_logger(), "Node ready.");
@@ -14,21 +14,21 @@ public:
   void move()
   {
     twist_.linear.x = 0.0;
-    twist_.angular.z = 0.1; // move left or right
-    for (int i = 1; (i < 5) && rclcpp::ok(); ++i) {
+    twist_.angular.z = 0.1; // move left 
+    for (int i = 1; (i < 3) && rclcpp::ok(); ++i) {
       publisher_->publish(twist_);
       loop_rate_.sleep(); 
-      // allow the robot to turn for 4 second
+      // allow the robot to turn for 5 second
     }
     twist_.angular.z = 0.0;
     publisher_->publish(twist_);
 
     twist_.linear.x = 0.5;   // Set forward linear velocity 
     //twist_.angular.z = 0.0;  // Set zero angular velocity (no rotation)
-    for (int i = 1; (i < 5) && rclcpp::ok(); ++i) {
+    for (int i = 1; (i < 3) && rclcpp::ok(); ++i) {
       publisher_->publish(twist_);
       loop_rate_.sleep();
-      // allow the robot to move forward for 4 second
+      // allow the robot to move forward for 5 second
     }
 
     twist_.linear.x = 0.0;
